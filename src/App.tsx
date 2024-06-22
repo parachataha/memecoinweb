86b3a554291e90a7cd2938c97eaf2e570421adbd
Sanyam Sindhu <89131737+sanyam-sindhu@users.noreply.github.com>
2024-06-22 17:15:11 +0530

    update check

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import {
   useLocation,
 } from "react-router-dom";
 import Home from "./pages/Home";
-import Particle from "./pages/Particle";
+
 
 function App() {
   
   return (
     <div>
-      <Particle/>
+    
       <Home/>
 
     </div>
